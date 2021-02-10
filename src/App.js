ae86b76cbf201bbdb17759c557888b4b8740ad77
Shan Siddiqui <shansiddiqui@Shans-Air.lan>
2021-02-10 18:37:41 -0500

    shan.s update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
-
+import SongFinder from "../src/components /SongFinder";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      foo
+      <SongFinder />
+    </div>
+  );
 }
 
 export default App;
