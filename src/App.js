b789aa6bd9cba3bd2d717ff8efd5f1fb7c1396d0
HaxagonusD <jquezad1@binghamton.edu>
2021-02-12 01:22:41 -0500

    Animation for about works

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import tw from "twin.macro";
 import "./App.css";
 import StrawberryButton from "./components/StrawberryButton";
 import { useSpring, animated } from "react-spring";
+import { useState, useEffect } from "react";
 function App() {
   // const dispatch = useDispatch();
   // const blobUrl = useSelector((state) => state.recorder.blobUrl);
@@ -11,9 +12,20 @@ function App() {
   const [props, setProps] = useSpring(() => ({
     transform: "translateY(0%)",
   }));
+  const [toggle, setToggle] = useState(false);
+
   return (
     <div tw="w-full h-screen overflow-hidden">
       <div
+        onClick={() => {
+          if (toggle) {
+            setToggle(false);
+            setProps({
+              transform: "translateY(50%)",
+              from: { transform: "translateY(50%)" },
+            });
+          }
+        }}
         className="App"
         tw="flex w-full h-screen justify-around items-center bg-melon flex-col "
       >
@@ -22,6 +34,7 @@ function App() {
           <div
             onClick={() => {
               setProps({ transform: "translateY(-50%)" });
+              setToggle(true);
             }}
             tw="text-white text-2xl"
           >
@@ -31,8 +44,15 @@ function App() {
         </div>
         <div tw="bg-white"></div>
       </div>
-      <animated.div style={props} tw="min-h-screen relative bg-blue-500">
-        <p tw="relative text-red-500">
+      <animated.div
+        style={{
+          ...props,
+        }}
+        tw="min-h-screen relative bg-white p-8 rounded"
+      >
+        <h1 tw="text-4xl mb-8">About</h1>
+
+        <p tw="text-black ">
           Ever not know the name of the song you’re listening to? Strawberry is
           here to help you out. Just press the Strawberry icon and in a few
           moments you will know the name of the song with lyrics to it too, if
